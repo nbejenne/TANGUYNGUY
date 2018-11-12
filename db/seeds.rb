9751d686af47342b64b288368905c546c02b011e
Nicolas Bejenne <nicolas.bejenne@gmail.com>
2018-11-12 19:07:18 +0100

    added seed and routes

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,59 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+
+
+User.destroy_all
+Spot.destroy_all
+# Booking.destroy_all
+# Feature.destroy_all
+# SpotFeature.destroy_all
+
+
+puts 'Creating seed'
+
+
+
+#   User.create!
+
+spots_attributes = [
+  user: User.new(
+    first_name: 'roger',
+    last_name: 'La bouteille',
+    nickname: 'Gégé',
+    email: 'gege@gege.com',
+    password: 'azerti',
+    ),
+  description: "Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un imprimeur anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte.",
+  photo: 'https://selfpackaging.fr/32197-imagenes_complementos/petite-maison-en-carton.jpg',
+  name: 'ma petite maison de papier',
+  address: '35 Rue du Cancera, 33000 Bordeaux'
+  ]
+
+Spot.create!(spots_attributes)
+
+feature_attributes = [
+{name: 'eau courante',
+picto: '*'},
+{name: 'electricité',
+picto: '*'},
+{name: 'wifi',
+picto: '*'},
+{name: 'TV',
+picto: '*'},
+{name: 'xbox',
+picto: '*'},
+{name: 'rechaud',
+picto: '*'},
+{name: 'serviette',
+picto: '*'}
+]
+
+Feature.create!(feature_attributes)
+# Ingredient.create!(ingredients_attributes)
+# cocktails_attributes = [
+#   { name: 'mojito', url: 'cocktails.jpg' },
+#   { name: "ti' punch", url: 'cocktails.jpg' },
+#   { name: "White Russian", url: 'cocktails.jpg'}
+# ]
+# Cocktail.create!(cocktails_attributes)
+
+puts 'Finished!'
