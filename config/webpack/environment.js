b5f0b8e8a889eaa621a9236c8b9f45bdc296ec2c
Nicolas Bejenne <nicolas.bejenne@gmail.com>
2018-11-16 17:02:42 +0100

    fix environment js

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,12 +1,26 @@
 const { environment } = require('@rails/webpacker')
-
-// Bootstrap 3 has a dependency over jQuery:
 const webpack = require('webpack')
+const dotenv = require('dotenv')
+
+const dotenvFiles = [
+  `.env.${process.env.NODE_ENV}.local`,
+  '.env.local',
+  `.env.${process.env.NODE_ENV}`,
+  '.env'
+]
+
+dotenvFiles.forEach((dotenvFile) => {
+  dotenv.config({ path: dotenvFile, silent: true })
+})
+
 environment.plugins.prepend('Provide',
   new webpack.ProvidePlugin({
     $: 'jquery',
     jQuery: 'jquery'
   })
 )
+
+environment.plugins.prepend('Environment', new webpack.EnvironmentPlugin(JSON.parse(JSON.stringify(process.env))))
+
 
 module.exports = environment
